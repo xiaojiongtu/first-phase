51f28f62e259c9985a5fae81a85aa4960a966677
xiaojiongtu <18377335170@163.com>
2018-12-10 17:10:57 +0800

    捕鱼达人1,2

---
+++ 2017-12-18/js/common.js
@@ -0,0 +1,26 @@
+function loadImages(json,fn) {
+	let res={};
+	let total=0;
+	let complete=0;
+	for(let name in json){
+		total++;
+		let oImg=new Image();
+		res[name]=oImg;
+		oImg.onload=function () {
+			complete++;
+			if (total==complete){
+				fn(res);
+			}
+		}
+		oImg.onerror =function () {
+			alert("图片加载失败"+oImg.src);
+		}
+		oImg.src=json[name];
+	}
+}
+function d2a(n) {
+	return n*Math.PI/180
+}
+function a2d(n) {
+	return n*180/Math.PI
+}
